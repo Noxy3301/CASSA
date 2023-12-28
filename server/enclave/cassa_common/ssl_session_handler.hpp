db6ef0336e1dc9756c01dc4af738a0aa0868509f
Noxy3301 <curelia@outlook.jp>
2023-12-28 22:48:27 +0900

    update: session ID (uint64_t -> std::string)

--- server/enclave/cassa_common/ssl_session_handler.hpp
+++ server/enclave/cassa_common/ssl_session_handler.hpp
@@ -3,6 +3,7 @@
 #include <openssl/ssl.h>
 #include <map>
 #include <vector>
+#include <string>
 
 #include "random.h"
 
@@ -11,21 +12,48 @@
 
 class SSLSessionHandler {
 public:
-    std::map<uint64_t, SSL*> ssl_sessions_; // session ID -> SSL session
+    std::map<std::string, SSL*> ssl_sessions_; // session ID -> SSL session
     Xoroshiro128Plus rnd_;  // random number generator
 
     SSLSessionHandler() {}
+
+    /**
+     * @brief Convert a number to a character
+     * @param[in] num number to convert
+     * @return char converted character
+    */
+    char number_to_char(uint64_t num) {
+        if (num < 26) {
+            return 'A' + num;
+        } else {
+            return '0' + (num - 26);
+        }
+    }
+
+    /**
+     * @brief Generate a session ID
+     * @return Session ID
+     * @note Session ID is a 6-character string (hard-coded)
+    */
+    std::string generateSessionID() {
+        std::string id;
+        for (int i = 0; i < 6; i++) {
+            id += number_to_char(rnd_.next() % 36);
+        }
+        return id;
+    }
 
     /**
      * @brief Add a new SSL session to the map
      * @param ssl(SSL*) SSL session
      * @return Session ID
     */
-    uint64_t addSession(SSL *ssl) {
-        uint64_t session_id;
+    std::string addSession(SSL *ssl) {
+        std::string session_id;
+        // repeat until a unique session ID is generated
         while (true) {
             // generate session ID
-            session_id = rnd_.next();
+            session_id = generateSessionID();
             if (ssl_sessions_.count(session_id) == 0) break; // check duplication
         }
         // add session to the map
@@ -35,26 +63,30 @@ public:
 
     /**
      * @brief Get SSL session from the map
-     * @param session_id(uint64_t) Session ID
+     * @param session_id(std::string) Session ID
      * @return SSL session (nullptr if not found)
     */
-    SSL *getSession(uint64_t session_id) {
+    SSL *getSession(std::string session_id) {
         if (ssl_sessions_.count(session_id) == 0) return nullptr;
         return ssl_sessions_[session_id];
     }
 
     /**
      * @brief Remove SSL session from the map
-     * @param session_id(uint64_t) Session ID
+     * @param session_id(std::string) Session ID
      * @param ssl_error_code(int) SSL error code
     */
-    void removeSession(uint64_t session_id, int ssl_error_code) {
+    void removeSession(std::string session_id, int ssl_error_code) {
         // get SSL session corresponding to the session ID
         auto it = ssl_sessions_.find(session_id);
         if (it == ssl_sessions_.end()) return;  // if not found, do nothing
 
         // error handling
         switch (ssl_error_code) {
+            case SSL_ERROR_NONE:
+                // this error code is returned when SSL_read/SSL_write succeeds
+                t_print(TLS_SERVER "SSL_ERROR_NONE\n");
+                break;
             case SSL_ERROR_SSL:
                 // this error code is returned when an error occurred 
                 // (e.g, protocol error, handshake failure)
@@ -67,12 +99,12 @@ public:
             case SSL_ERROR_SYSCALL:
                 // this error code is returned when the client closes 
                 // the connection without sending a close_notify alert
-                t_print(TLS_SERVER "Session ID: %lu may have closed unexpectedly (SSL_ERROR_SYSCALL).\n", session_id);
+                t_print(TLS_SERVER "Session ID: %s may have closed unexpectedly (SSL_ERROR_SYSCALL).\n", session_id.c_str());
                 break;
             case SSL_ERROR_ZERO_RETURN:
                 // this error code is returned when the client closes 
                 // the connection with sending a close_notify alert
-                t_print(TLS_SERVER "Session ID: %lu has closed (SSL_ERROR_ZERO_RETURN).\n", session_id);
+                t_print(TLS_SERVER "Session ID: %s has closed (SSL_ERROR_ZERO_RETURN).\n", session_id.c_str());
                 break;
             default:
                 t_print(TLS_SERVER "Unknown error code: %d\n", ssl_error_code);

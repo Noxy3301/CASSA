13b5abef93a70634f5c517f71ef5824a7e5c3ba8
Noxy3301 <curelia@outlook.jp>
2024-01-18 03:16:41 +0900

    update: open file mode

--- server/app/app_server.cpp
+++ server/app/app_server.cpp
@@ -88,12 +88,19 @@ void print_error_message(sgx_status_t ret) {
         printf("Error code is 0x%X. Please refer to the \"Intel SGX SDK Developer Reference\" for more details.\n", ret);
 }
 
-void write_sealData(std::string filePath, const uint8_t* sealed_data, const size_t sealed_size) {
-    std::ofstream file(filePath, std::ios::out | std::ios::binary);
+void write_sealData(std::string filePath, const uint8_t* sealed_data, const size_t sealed_size, bool append = false) {
+    // setting open mode
+    std::ios_base::openmode mode = std::ios::out | std::ios::binary;
+    if (append) mode |= std::ios::app;  // append
+
+    // open file
+    std::ofstream file(filePath, mode);
     if (file.fail()) {
         perror("file open failed");
         abort();
     }
+
+    // write
     file.write((const char*)sealed_data, sealed_size);
     file.close();
 }
@@ -101,7 +108,7 @@ void write_sealData(std::string filePath, const uint8_t* sealed_data, const size
 int ocall_save_logfile(const uint8_t* sealed_data, const size_t sealed_size) {
     int thid = 0;   // TODO: セッションがどのファイルに書き込むかを検討する
     std::string filePath = "log/log" + std::to_string(thid) + ".seal";
-    write_sealData(filePath, sealed_data, sealed_size);
+    write_sealData(filePath, sealed_data, sealed_size, true);
     return 0;
 }
 

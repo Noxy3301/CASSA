fba71b3500f483dafc0bf7903aec459cd8dfa7df
Noxy3301 <curelia@outlook.jp>
2024-02-03 09:54:02 +0900

    add: log_macros.h

---
+++ common/log_macros.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "ansi_color_code.h"
+
+#define LOG_INFO  GRN "[ INFO     ] " CRESET
+#define LOG_WARN  YEL "[ WARNING  ] " CRESET
+#define LOG_ERROR RED "[ ERROR    ] " CRESET
+#define LOG_FATAL MAG "[ FATAL    ] " CRESET
+#define LOG_DEBUG CYN "[ DEBUG    ] " CRESET
+
+#define LOG_SPACE     "             "
